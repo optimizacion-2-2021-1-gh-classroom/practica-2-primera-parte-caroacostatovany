effa4a3b0154403d9750f9792a095b34e2b954c6
Eduardo Moreno <e_mortiz23@hotmail.com>
2021-05-01 18:50:42 -0500

    debuggeando seccion tests #5

--- src/tests/test.py
+++ src/tests/test.py
@@ -3,8 +3,9 @@ from pytest import approx
 from scipy.optimize import linprog
 
 import os
-os.chdir("..")
-os.system("setup.py")
+#os.chdir("..")
+#os.system("setup.py")
+os.system('pip install "git+https://github.com/optimizacion-2-2021-1-gh-classroom/practica-1-segunda-parte-caroacostatovany.git#egg=mex&subdirectory=src"')
 
 from mex.simplex.simplex_networks import create_matrix
 from mex.simplex.problem_definition import constrain, obj, minz, maxz
