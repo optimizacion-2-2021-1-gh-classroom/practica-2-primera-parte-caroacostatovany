1682a1c5fe178f20a4458186799fd10c7e280336
Eduardo Moreno <e_mortiz23@hotmail.com>
2021-05-01 18:43:47 -0500

    Debuggeando pruebas #5

--- src/tests/test.py
+++ src/tests/test.py
@@ -2,9 +2,9 @@ import numpy as np
 from pytest import approx
 from scipy.optimize import linprog
 
-#import os
-#os.chdir("..")
-#os.system(setup.py)
+import os
+os.chdir("..")
+os.system("setup.py")
 
 from mex.simplex.simplex_networks import create_matrix
 from mex.simplex.problem_definition import constrain, obj, minz, maxz
