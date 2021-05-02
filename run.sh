446cc3726de6455e06a848d08e557d5d83724aa2
caroacostatovany <carolina.acostatovany@gmail.com>
2021-05-01 19:55:45 -0500

    dockerfile para binder

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-NB_USER=jovyan
-HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b master --single-branch https://github.com/optimizacion-2-2021-1-gh-classroom/practica-2-primera-parte-caroacostatovany.git
-
-exec "$@"
